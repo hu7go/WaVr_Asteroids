1b4f8c9e1494683d49823d276c39b6f91e7e42ce
Hugo Cremer <slasken49@gmail.com>
2019-01-11 04:38:37 +1400

    Commit

--- WaVr - Copy/Assets/Scripts/StayingY.cs
+++ WaVr - Copy/Assets/Scripts/StayingY.cs
@@ -4,11 +4,8 @@ public class StayingY : MonoBehaviour
 {
     public Transform target;
 
-    public float x = 0;
-    public float z = 0;
-
 	void LateUpdate ()
     {
-        transform.localRotation = Quaternion.Euler(x, target.parent.transform.localRotation.eulerAngles.y, z);
+        transform.localRotation = Quaternion.Euler(0, target.parent.transform.localRotation.eulerAngles.y, 0);
 	}
 }
