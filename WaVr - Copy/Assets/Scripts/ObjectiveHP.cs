6e8c3272fa8040b8b329bcb37db7d6b658c28e19
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2019-01-17 16:19:42 +0100

    readjutslider and try with objectivehp ignore raycast from hand

--- WaVr - Copy/Assets/Scripts/ObjectiveHP.cs
+++ WaVr - Copy/Assets/Scripts/ObjectiveHP.cs
@@ -24,5 +24,10 @@ public class ObjectiveHP : MonoBehaviour {
             Manager.Instance.slider.value = Hp;
             CheckHP();
         }
+
+        if (!other.CompareTag("bullet"))
+        {
+            return;
+        }
     }
 }
