a9b849df443524d137437d6ca4742654e5b7d20b
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2019-01-21 10:16:21 +0100

    SpinningUnicorn

---
+++ WaVr - Copy/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+    float random;
+
+	void Start () {
+        random = Random.Range(0.1f, 2f);
+	}
+	
+	void Update () {
+        transform.RotateAround(Vector3.zero,Vector3.right,random * Time.deltaTime);
+	}
+}
