37739e1e2618c4e98b1264e68983fe9055042667
Hugo Cremer <slasken49@gmail.com>
2019-01-28 23:55:47 +1400

    Commit

---
+++ WaVr - Copy/Assets/Scripts/Teleport/HeadsetRotation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadsetRotation : MonoBehaviour
+{
+    public Transform headset;
+    public float minAngle = 40;
+
+    private TeleportMaster teleportMaster;
+    private bool rotated;
+
+    public ChangeSide[] arrows;
+
+    private void Start()
+    {
+        teleportMaster = GetComponent<TeleportMaster>();
+    }
+
+    private void Update()
+    {
+        if ((headset.localRotation.eulerAngles.x >= minAngle && headset.localRotation.eulerAngles.x <= 180) && !rotated)
+        {
+            rotated = true;
+            StartCoroutine(Test());
+            arrows[0].DoFunction();
+            return;
+        }
+        if ((headset.localRotation.eulerAngles.x <= 360 - minAngle && headset.localRotation.eulerAngles.x >= 180) && !rotated)
+        {
+            rotated = true;
+            StartCoroutine(Test());
+            arrows[1].DoFunction();
+            return;
+        }
+        if ((headset.localRotation.eulerAngles.z >= minAngle && headset.localRotation.eulerAngles.z <= 180) && !rotated)
+        {
+            rotated = true;
+            StartCoroutine(Test());
+            arrows[2].DoFunction();
+            return;
+        }
+        if ((headset.localRotation.eulerAngles.z <= 360 - minAngle && headset.localRotation.eulerAngles.z >= 180) && !rotated)
+        {
+            rotated = true;
+            arrows[3].DoFunction();
+            StartCoroutine(Test());
+            return;
+        }
+    }
+
+    IEnumerator Test ()
+    {
+        yield return new WaitForSeconds(1f);
+        rotated = false;
+    }
+}
