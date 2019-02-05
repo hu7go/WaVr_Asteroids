cb733737f751f2c6334981a544fe2773a30e5586
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2019-02-05 15:05:06 +0100

    heartRotation

---
+++ WaVr - Copy/Assets/Scripts/Teleport/HeartRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class HeartRotation : MonoBehaviour
+{
+    void Update()
+    {
+        if(Manager.Instance.turretsAndEnemies.heartrotator)
+            gameObject.transform.LookAt(Manager.Instance.ReturnPlayer().transform);
+        if (!Manager.Instance.turretsAndEnemies.heartrotator)
+            gameObject.transform.Rotate(1, 0, 0);
+    }
+}
