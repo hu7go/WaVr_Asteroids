4f057f79b2fd219215196fe5c4078b191ebc30c5
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2019-02-05 16:06:52 +0100

    pushesBepushiing

--- WaVr - Copy/Assets/Scripts/Teleport/HeartRotation.cs
+++ WaVr - Copy/Assets/Scripts/Teleport/HeartRotation.cs
@@ -2,10 +2,12 @@
 
 public class HeartRotation : MonoBehaviour
 {
+    //public GameObject looker;
     void Update()
     {
         if(Manager.Instance.turretsAndEnemies.heartrotator)
             gameObject.transform.LookAt(Manager.Instance.ReturnPlayer().transform);
+            //gameObject.transform.LookAt(looker.transform);
         if (!Manager.Instance.turretsAndEnemies.heartrotator)
             gameObject.transform.Rotate(0, 1, 0);
     }
