d9ecd5e5a03f0c95f07d8be46c5939074bcb7f8a
Hugo Cremer <slasken49@gmail.com>
2019-02-13 02:23:45 +1400

    Revert "Fixed new version of cSharp"
    This reverts commit 1eec445dd0abb88b2c38b3321763445a29753696.

--- WaVr - Copy/Assets/Oculus/Avatar/Scripts/OvrAvatarSkinnedMeshRenderPBSV2Component.cs
+++ WaVr - Copy/Assets/Oculus/Avatar/Scripts/OvrAvatarSkinnedMeshRenderPBSV2Component.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System;
+using System.Security.Policy;
 using Oculus.Avatar;
 
 public class OvrAvatarSkinnedMeshPBSV2RenderComponent : OvrAvatarRenderComponent
