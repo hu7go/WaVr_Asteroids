96adb3a078090792e65a3c97c13d65d4a3657a5b
Hugo Cremer <slasken49@gmail.com>
2019-02-15 04:59:46 +1400

    Commit

--- WaVr - Copy/Assets/_Scripts/ReloadScene.cs
+++ WaVr - Copy/Assets/_Scripts/ReloadScene.cs
@@ -3,9 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class ReloadScene : MonoBehaviour
 {
+    int wv = 1;
+
     void Update()
     {
         if(Input.GetKeyDown("r"))
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        if (Input.GetKeyDown("t"))
+        {
+            switch (wv)
+            {
+                case 1:
+                    Manager.Instance.graphicsSettings.worldVersion = Manager.GraphicsSettings.WorldVersion.one;
+                    break;
+                case 2:
+                    Manager.Instance.graphicsSettings.worldVersion = Manager.GraphicsSettings.WorldVersion.two;
+                    break;
+                case 3:
+                    Manager.Instance.graphicsSettings.worldVersion = Manager.GraphicsSettings.WorldVersion.three;
+                    wv = 0;
+                    break;
+            }
+
+            Manager.Instance.SetWorldVersion();
+
+            wv++;
+        }
     }
 }
