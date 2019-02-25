4e049e239d96ea9978d7699abb661fcb14e9f460
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2019-02-25 11:31:10 +0100

    scriptable object updated

--- WaVr - Copy/Assets/ScriptableObjects/Enemies/Enemies.cs
+++ WaVr - Copy/Assets/ScriptableObjects/Enemies/Enemies.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu()]
 public class Enemies : ScriptableObject
 {
+    public new string name;
+    public bool boss;
     public GameObject enemie;
+    public float damage;
+    public float health;
+    public float speed;
+    public Vector3 scale;
+    public Mesh mesh;
 }
