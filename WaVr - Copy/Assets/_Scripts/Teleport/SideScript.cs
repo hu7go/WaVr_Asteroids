be005c4c5e6b2d96e3dd1fedaa1b7214894dbd1a
Hugo Cremer <slasken49@gmail.com>
2019-02-27 22:40:32 +1400

    Commit

--- WaVr - Copy/Assets/_Scripts/Teleport/SideScript.cs
+++ WaVr - Copy/Assets/_Scripts/Teleport/SideScript.cs
@@ -8,8 +8,35 @@ public class SideScript : MonoBehaviour
     [SerializeField] private Transform teleportPos;
     public Transform rotator;
     public Vector3 sideOffset;
+    public bool highlightBool = false;
+    public MeshRenderer highlight;
+    private Transform player;
 
-    private void Start() => parent = GetComponentInParent<Asteroid>();
+    private float distance;
+    private float maxDistance;
+
+    private void Start()
+    {
+        parent = GetComponentInParent<Asteroid>();
+        player = Manager.Instance.ReturnPlayer().transform;
+        maxDistance = Manager.Instance.tpMaster.GetMaxLenght();
+    }
+
+    void Update()
+    {
+        if (highlightBool)
+        {
+            //The + 3 is the radius of the sphere collider!
+            distance = (transform.position - player.position).magnitude - 3;
+
+            //This might be not good at all!
+            //Probably(maybe) is better to do once when you teleport!
+            if (distance <= maxDistance)
+                highlight.enabled = true;
+            else
+                highlight.enabled = false;
+        }
+    }
 
     public void Reached () => parent.Reached();
 
