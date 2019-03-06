c261449567504eeb876280610592012e1ab34c76
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2019-03-06 10:51:56 +0100

    stuff and things

---
+++ WaVr - Copy/Assets/_Scripts/Enemy/EAI_Suicider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EAI_Suicider : EnemyAI
+{
+    public GameObject deathEffect2;
+    public override void Initialize(List<AsteroidHealth> newList, float newHealthThreshHold, EnemySpawnPoint newMaster, Spawner newSpawner, int newWaveIndex, Enemies enemyType)
+    {
+        base.Initialize(newList, newHealthThreshHold, newMaster, newSpawner, newWaveIndex, enemyType);
+    }
+    public override void Movement()
+    {
+        base.Movement();
+        if(distance < 1.5f)
+        {
+            objective.asteroid.health -= gun.damage;
+            Instantiate(deathEffect, transform.position, transform.rotation);
+            Instantiate(deathEffect2, transform.position, transform.rotation);
+            Manager.Instance.tAe.killCount++;
+            Kill();
+        }
+    }
+    public override void TakeDamage(int damage)
+    {
+        health -= (int)gun.damage;
+        if (health <= 0)
+        {
+            if (distance < 2f)
+                objective.asteroid.health -= damage;
+
+            Manager.Instance.tAe.killCount++;
+            Instantiate(deathEffect, transform.position, transform.rotation);
+            Instantiate(deathEffect2, transform.position, transform.rotation);
+            Kill();
+        }
+    }
+}
