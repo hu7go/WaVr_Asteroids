6bdf4300cda70cb994e8ea0fd54032002d4632d0
Hugo Cremer <slasken49@gmail.com>
2019-03-08 01:43:44 +1400

    commit

--- WaVr - Copy/Assets/ScriptableObjects/WaveTriggers/Wave.cs
+++ WaVr - Copy/Assets/ScriptableObjects/WaveTriggers/Wave.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 [System.Serializable]
 public class Wave
 {
+    public int index = 0;
     public float damageThreshHold;
     public Transform spawnPosition;
     public SO_EnemyWave enemyController;
+    public bool extraWaitTimeBool = true;
+    public float extraWaitTime = 10f;
+    [Tooltip("This time will only be in effect if the time trigger is active!")]
     public float waitTime = 0;
     [HideInInspector] public float currentNumberOfEnemies;
     [HideInInspector] public List<EnemyAI> enemies;
     [HideInInspector] public float damageDone;
-    public int index = 0;
 
     [Tooltip("BEWARE!! This should not be more than there are triggers in the triggers list!!!")]
     public int numberOfrequiredTriggers = 1;
