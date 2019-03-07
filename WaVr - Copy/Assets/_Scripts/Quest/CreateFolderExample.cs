526377426195b4f970d6eef0ed55824917f9a6ea
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2019-03-07 14:07:18 +0100

    update for fun

---
+++ WaVr - Copy/Assets/_Scripts/Quest/CreateFolderExample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateFolderExample : MonoBehaviour
+{
+    [MenuItem("GameObject/Create Folder")]
+    private void Start()
+    {
+        print("step one");
+        if(!AssetDatabase.IsValidFolder("Assets/HUGOMAPPEN"))
+        {
+            print("Step Two");
+            string guid = AssetDatabase.CreateFolder("Assets", "HUGOMAPPEN");
+            string newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
+        }
+    }
+}
